c0b7d91bae3625a32c9003e344a8d6dc55bb1755
montermarco <monter.marco@gmail.com>
2022-05-15 17:36:48 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole('button', { name: 'change to blue' });
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red'});
+  
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  // expect(colorButton.textContent).toBe('change to red');
+
+});
+
+test('button has correct initial text', () => {
+});
+
+test('button turns to blue when clicked', () => {
 });
